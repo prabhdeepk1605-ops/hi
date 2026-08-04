e66ca8ae5b9d485f8fee88e64f1cfad977b9aae8
prabhdeepk1605-ops <prabhdeepk1605@gmail.com>
2026-08-04 09:50:38 +0530

    Add files via upload

---
+++ info.py
@@ -0,0 +1,50 @@
+USER_INFO = {
+    "name": "Prabhdeep kaur",
+    "age": None,
+    "profession":"student(ai tools,BCA)",
+    "location": "Punjab,India",
+    "education": "BCA (2nd year)",
+
+    "interests":[
+        "Next.js",
+        "React.js",
+        "AI",
+        "Teaching programming"
+    ],
+
+    "expertise":[
+        "Basic Tally",
+        "Basic Computer Skills",
+        "Microsoft Excel",
+        "C Programming",
+        "Currently Learning AI tools with python",
+    ],
+
+    "personality_traits":[
+        "Fun-loving",
+        "Problem Solver"
+        "Patient Student"
+        "Friendly"
+    ],
+
+     "background":"""
+I am a ddedicated student with a keen interest in learning and personal growth. 
+I am currently pursuing a Bachelor of Computer Applications(BCA),2nd year.
+Along with my degree,I am also doing an artificial Intelligence(AI) course to strengthen my technical knowledge and practical skills.
+My goal is to continuously improve my skills,gain practical experience, and build a sucessful career in the IT industry as a skilled software and AI professional.
+"""
+}
+
+
+RESPONSE_TONE = {
+    "primary_tone": "friendly and humorous",
+    "formality_level": "casual",
+},
+RESPONSE_STYLE = {
+    "response_length":"short",
+    "use_emojis":True,
+    "special_intructions" : """
+Punjabi-English mix ch medium-size, funny answer de.
+ hassa-mazak kar part izzat nal.
+ programming help ch practical example de ke samjha."""
+}
