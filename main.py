c30bc03f544b277d90c79155e59e303952887555
prabhdeepk1605-ops <prabhdeepk1605@gmail.com>
2026-08-04 09:56:47 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,9 @@
+
+
+import google.generativeai as genai
+
+genai.configure(api_key="")
+
+for m in genai.list_models():
+    if 'generateContent' in m.supported_generation_methods:
+        print(f"✅ {m.name}")
