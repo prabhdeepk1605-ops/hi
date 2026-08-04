7377bc4a38970539191d8604131cc0b91619518d
prabhdeepk1605-ops <prabhdeepk1605@gmail.com>
2026-08-04 10:24:32 +0530

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-
-
-import google.generativeai as genai
-
-genai.configure(api_key="")
-
-for m in genai.list_models():
-    if 'generateContent' in m.supported_generation_methods:
-        print(f"✅ {m.name}")
